04339ce252fb3c4bf49343ad84288dfccea39d55
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-02-03 12:34:36 +0500

    update

--- adamallys/next.config.mjs
+++
@@ -1,9 +0,0 @@
-
-const nextConfig = {
-  /* config options here */
-  images: {
-    domains: ['s3-alpha-sig.figma.com', "res.cloudinary.com"], // Add the domain here
-  },
-};
-
-export default nextConfig;
