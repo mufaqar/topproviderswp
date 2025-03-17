80f697f7b47ce9543365975f8e32f465d7b4e46f
mufaqar <mufaqar@gmail.com>
2025-03-17 16:45:40 +0500

    Feedback

--- template-parts/client-reviews.php
+++ template-parts/client-reviews.php
@@ -3,21 +3,21 @@
         <div class="bg-[#044FC3] p-4 flex text-xl text-white w-fit mb-2 rounded-xl">
             <i class="fa fa-quote-left"></i>
         </div>
-        <p class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eius aliquid aut, vitae voluptates facilis explicabo non a! Quod dolore iste debitis, doloremque dolor sunt!</p>
-        <h6 class="mt-2 text-[#044FC3]">Danial</h6>
+        <p class="text-lg text-gray-600">I found TopProviders.net to be extremely helpful in comparing internet providers in the USA. The website is user-friendly, and the filters made it easy to narrow down options based on speed, price, and availability in my area. The detailed provider reviews helped me make an informed decision. Highly recommended!</p>
+        <h6 class="mt-2 text-[#044FC3]">Emma Watson</h6>
     </div>
     <div class="shadow-xl p-8 rounded-xl border">
         <div class="bg-[#044FC3] p-4 flex text-xl text-white w-fit mb-2 rounded-xl">
             <i class="fa fa-quote-left"></i>
         </div>
-        <p class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eius aliquid aut, vitae voluptates facilis explicabo non a! Quod dolore iste debitis, doloremque dolor sunt!</p>
-        <h6 class="mt-2 text-[#044FC3]">Danial</h6>
+        <p class="text-lg text-gray-600">The website is great for comparing internet providers, but I noticed that some pricing details were outdated compared to the official provider websites. It would be even more useful if they ensured all plans and prices were updated frequently. Other than that, the comparison tool is very handy!</p>
+        <h6 class="mt-2 text-[#044FC3]">Job William</h6>
     </div>
     <div class="shadow-xl p-8 rounded-xl border">
         <div class="bg-[#044FC3] p-4 flex text-xl text-white w-fit mb-2 rounded-xl">
             <i class="fa fa-quote-left"></i>
         </div>
-        <p class="text-lg text-gray-600">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia eius aliquid aut, vitae voluptates facilis explicabo non a! Quod dolore iste debitis, doloremque dolor sunt!</p>
-        <h6 class="mt-2 text-[#044FC3]">Danial</h6>
+        <p class="text-lg text-gray-600">I like the idea of comparing providers in one place, but the search feature could be improved. It would be great if I could enter my zip code directly on the homepage and instantly see available options. Adding customer reviews for each provider would also be helpful!</p>
+        <h6 class="mt-2 text-[#044FC3]">Kat Kattie</h6>
     </div>
 </section>
