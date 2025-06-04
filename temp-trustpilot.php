9c9e926e619e94864a5a863a72c5db40a4f44ec4
Mufaqar <mufaqar@gmail.com>
2025-06-04 16:55:45 +0500

    updated

--- temp-trustpilot.php
+++ temp-trustpilot.php
@@ -1,173 +1,47 @@
 <?php
-/**
- * The front page file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package Built_By_Naif
- */
+/** Template Name: Trust Reviews */
 
 get_header();
 ?>
 
-<img src="<?php echo get_theme_file_uri('/images/bob-dylan.gif'); ?>">
-<div class="trigger" id="about-us" data-matching-link="about-us-link">
-    <div class="dylan-flex">
-      <div class="dylan-flex--inner">
-          <div class="video-responsive">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/YA1zc0XRKjY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-      </div>
-      <div class="dylan-flex--inner guitar">
-          <h2>Man in Me (Bob Dylan Cover) by Nondon Bagchi</h2>
-          <p>"The man in me will do nearly any task And as for compensation, there's little he would ask."</p>
-<p>Calcutta's beloved wordsmith, drummer, teacher and food critic, Nondon Bagchi sings Bob Dylan, making the words sound like his autobiographical account.</p>
-<p>Completing over 50 years of musicianship, Bagchi is the dark horse who sacrificed his materialistic aspirations to unconditionally serve his city with an assortment of expertise</p>
-          <p><strong>Vocals:</strong> Nondon Bagchi</p>
-          <p><strong>Guitar:</strong> Jaimin Rajani</p>
-      </div>
+<section class="py-24 bg-[#6041BB]">
+    <div class="container mx-auto px-4">
+        <h1 class="sm:text-5xl text-4xl  font-semibold  text-white text-center"><?php the_title()?></h1>
     </div>
-    <hr class="dylan-hr">
-    <div class="dylan-flex--inverse">
-      <div class="dylan-flex--inner">
-           <div class="video-responsive">
-             <iframe width="560" height="315" src="https://www.youtube.com/embed/qc3IbVrHyEs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-      </div> 
-      <div class="dylan-flex--inner piano">
-          <h2>Blowin' in the wind - Usha Uthup</h2>
-          <p>Listen to Calcutta's People's singer, Usha Uthup cover Dylan's magnum opus, Blowin' in the Wind, which became perhaps the most important song of the Civil Rights Movement in the '60s and was further elevated to become an International Anthem.</p>
-       
-        
-      </div>
-    </div>
-    <hr class="dylan-hr">
-    <div class="dylan-flex">
-      <div class="dylan-flex--inner">
-           <div class="video-responsive">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/kFGsqvuFIpY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-      </div> 
-      <div class="dylan-flex--inner harmonica">
-          <h2>Hey Bob Dylan</h2>
-          <p>Hey Bob Dylan was written in 2000 as a tribute and was later recorded for the album Public Issue released in 2005. Thank you Opu and Rachael for compiling the song to a video for Bob Dylan's 75th. Happy birthday Bobby.</p>
-      </div>
-    </div>
-    <hr class="dylan-hr">
-    <div class="dylan-flex--inverse">
-      <div class="dylan-flex--inner">
-          <div class="video-responsive">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/ry0Y7FlIRhs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-      </div> 
-      <div class="dylan-flex--inner guitar">
-          <h2>Don't Think Twice It's All Right - Bob Dylan</h2>
-          <p>"You're the reason I'm travellin' on But don't think twice, it's all right"</p>
-          <p>Those of you who frequented Someplace Else on Park Street during the hay days of Hip Pocket, have probably already made the acquaintance of this Bob Dylan classic in the voice of our beloved wordsmith, Nondon Bagchi. Here's a recent recording of Bagchi, making those words his own.</p>
-          <p><strong>Vocals:</strong> Nondon Bagchi</p>
-          <p><strong>Guitar: </strong> Jaimin Rajani</p>
-        
-      </div>
-    </div>
-	<hr class="dylan-hr">
-	  <div class="dylan-flex">
-      <div class="dylan-flex--inner">
-          <div class="video-responsive">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/zuDE9kbPXWA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-      </div> 
-      <div class="dylan-flex--inner piano">
-          <h2>Rahul Guha Roy - One More Cup Of Coffee (Bob Dylan cover)</h2>
-          <p>Folk-Rock artist Rahul Guha Roy of Cassini's Division released his rendition of Dylan's One More Cup of Coffee from Desire, as part of his 2016 album Portraiture.</p>
-     
-        
-      </div>
-    </div>
-	<hr class="dylan-hr">
-	 <div class="dylan-flex--inverse">
-      <div class="dylan-flex--inner">
-          <div class="video-responsive">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/weiVyhUZi9k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-      </div> 
-      <div class="dylan-flex--inner harmonica">
-          <h2>You're a Big Girl Now (Bob Dylan Cover) - Jaimin Rajani</h2>
-          <p>Here's a cover of a Bob Dylan classic from his celebrated 1975 album Blood on the Tracks. Jaimin is a singer-songwriter and the writer of the Bob Dylan film - If Not for You (2019).</p>
-        
-      </div>
-    </div>
-	<hr class="dylan-hr">
-	<h2 class="centered">Dr. Shobana Mathews on Bob Dylan</h2>
-	<hr class="dylan-hr">
-    <div class="dylan-flex">
-      <div class="dylan-flex--inner">
-          <div class="video-responsive">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/NIKwtz7sQ80" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-      </div> 
-      <div class="dylan-flex--inner guitar">
-<!--           <h2>Don't Think Twice It's All Right - Bob Dylan</h2> -->
-          <p>Dr. Shobana Mathews is an Associate Professor in the Department of English Studies at CHRIST (Deemed to be University), Bengaluru. Her MPhil research in 1996 was titled Folksong as Protest: A Study of the Works of Bob Dylan.</p>
-        
-      </div>
-    </div>
-	
-	
-	<hr class="dylan-hr">
-	<h2 class="centered">Bob Dylan Tribute</h2>
-	<hr class="dylan-hr">
-	<div id="tribute" data-matching-link="tribute-link"></div>
-    <div class="dylan-flex--inverse">
-      <div class="dylan-flex--inner">
-          <div class="video-responsive">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/C96wuptPbe0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-      </div> 
-      <div class="dylan-flex--inner harmonica">
-		  <h2>Acoustic Tribute by Rachit Lakhmani</h2>
-          <p>Listen to Rachit Lakhmani sing Bob Dylan classics on the occasion of Dylan's 79th birth anniversary on May 24, 2020. Lakhmani is a singer-songwriter and an advocate at the Calcutta High Court.</p>
-        
-      </div>
-    </div>
-</div>
-<hr class="dylan-hr">
-<h2 class="centered">Playlist</h2>
-<hr class="dylan-hr"><br>
-<div id="playlist" data-matching-link="playlist-link"></div>
- <div class="video-responsive">
-      <iframe src="https://open.spotify.com/embed/playlist/1AjPqQEeXWsE2l7jklJUOM" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-   </div>
-<br><hr class="dylan-hr"><div id="video" data-matching-link="video-link"></div>
-<h2 class="centered">If Not for You - A Bob Dylan Film (2019)<a href="https://www.imdb.com/title/tt10418976/" target="_blank"><img class="imdb" src="https://bobdylan.in/wp-content/uploads/2020/05/imdb.png"></a></h2>
-<hr class="dylan-hr"><br>
-   <div class="video-responsive">
-       <iframe src="https://player.vimeo.com/video/338406342" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-<p><a href="https://vimeo.com/338406342">If Not for You - A Bob Dylan Film (2019)</a> from <a href="https://vimeo.com/thruthelens">Vineet Arora</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
-   </div>
-   <h2 class="centered">A film by Jaimin Rajani and Vineet Arora</h2>
-<p class="synopsis"><strong>Synopsis:</strong> Narrated by veteran actor, Dhritiman Chaterji, If Not for You is a tribute film depicting Calcutta's long-lasting love affair with legendary singer-songwriter, Bob Dylan through interviews of notable Indian musicians who were inspired by the bard.</p><br>
-<p class="synopsis"><strong>Featuring:</strong> Purna Das Baul, Anjan Dutt, Usha Uthup, Susmit Bose, Miti Adhikari, Nondon Bagchi, Arko Mukhaerjee, Dr. Ananda Lal and Rahul Guha Roy.</p><br><br>
+</section>
 
-<p class="synopsis centered"><em><strong>&#8220;</strong>It is very lovely and captures the magic and mystery of the man and his music. Its simplicity is simply compelling. <strong>&#8221;</strong></em></p>
-<p class="centered"><strong>Pritish Nandy</strong> (Filmmaker, Journalist, Poet)</p><br>
-<p class="synopsis centered"><em><strong>&#8220;</strong> A worthy project. Really well made <strong>&#8221;</strong></em></p>
-<p class="centered"><strong>Jeff Rosen</strong> (Bob Dylan's Manager &amp; Producer)</p><br>
-<p class="synopsis centered"><em><strong>&#8220;</strong> I was very moved by your wonderful Documentary on Bob Dylan, If Not for You, and your understanding of the man and his poetry. <strong>&#8221;</strong></em></p>
-<p class="centered"><strong>Scarlet Rivera</strong> (Violinist on Bob Dylan's Rolling Thunder Revue and Desire)</p><br>
-<p class="synopsis centered"><em><strong>&#8220;</strong> The filmmakers' dedication for Dylan, strums through every frame <strong>&#8221;</strong></em></p>
-<p class="centered"><strong>Bachi Karkaria</strong> (Journalist and Columnist)</p><br><br>
-<div class="logos-flex">
-	<div class="logos-flex--inner"><a href="https://www.thehindu.com/entertainment/music/a-documentary-tracks-kolkatas-long-lasting-love-affair-with-bob-dylan/article31110327.ece" target="_blank"><img src="https://bobdylan.in/wp-content/uploads/2020/05/hindu.png"></a></div>
-	<div class="logos-flex--inner"><a href="https://www.telegraphindia.com/culture/tata-steel-kolkata-literary-meet-2020-bob-dylan-and-the-beatles-were-much-more-than-their-music/cid/1742308" target="_blank"><img src="https://bobdylan.in/wp-content/uploads/2020/05/telegraph2.png"></a></div>
-	<div class="logos-flex--inner"><a href="https://timesofindia.indiatimes.com/entertainment/bengali/music/dylan-fans-of-the-city-make-a-documentary-on-the-legend/articleshow/71163716.cms" target="_blank"><img src="https://bobdylan.in/wp-content/uploads/2020/05/toi.png"></a></div>
-	<div class="logos-flex--inner"><a href="https://epaper.thestatesman.com/2403772/Marquee/9TH-NOVEMBER-2019#dual/1/1" target="_blank"><img src="https://bobdylan.in/wp-content/uploads/2020/05/statesman.jpg"></a></div>
-	<div class="logos-flex--inner"><a href="https://scroll.in/reel/957580/documentary-if-not-for-you-finds-that-bob-dylan-is-like-a-local-resident-of-calcutta" target="_blank"><img src="https://bobdylan.in/wp-content/uploads/2020/05/scroll.png"></a></div>
-	<div class="logos-flex--inner"><a href="https://www.indiatoday.in/magazine/leisure/story/20200629-bobindra-sangeet-1691153-2020-06-21" target="_blank"><img src="https://bobdylan.in/wp-content/uploads/2020/06/india-today.png"></a></div>
-</div>
-<?php
-get_footer();
+<section class="py-16">
+    <div class="container mx-auto px-4">
+     
+            <h3 class="text-xl sm:text-3xl text-center sm:text-left font-semibold mb-10">Trustpilot Reviews for Providers</h3>
+            <div class="mt-8 grid grid-cols-1 gap-4 text-center">
+                <?php
+                // Arguments for the WP Query
+                $args = array(
+                    'post_type'      => 'providers', // Custom post type name
+                    'posts_per_page' => -1, // Number of posts to display
+                    'order'          => 'DESC', // Order of the posts
+                    'orderby'        => 'date' // Order by date
+                );
+
+                // Custom query to fetch posts
+                $providers_query = new WP_Query($args);
+
+                // The Loop
+                if ($providers_query->have_posts()) :
+                    while ($providers_query->have_posts()) : $providers_query->the_post();
+                        get_template_part('template-parts/review', 'provider');
+                    endwhile;
+                else :
+                    echo '<p>No providers found.</p>';
+                endif;
+
+                // Reset post data to avoid conflicts
+                wp_reset_postdata();
+                ?>
+
+          
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
