9616477a630fdd212ed746cd3a2229f10d9979dd
Mufaqar <mufaqar@gmail.com>
2025-06-18 13:25:41 +0500

    Fast widget

--- template-parts/section/fast-providers.php
+++ template-parts/section/fast-providers.php
@@ -1,6 +1,6 @@
 <?php
 
 // Include Popular Posts Widget
-require get_template_directory() . '/inc/widgets/tabs-widget.php';
+//require get_template_directory() . '/inc/widgets/tabs-widget.php';
 
 
